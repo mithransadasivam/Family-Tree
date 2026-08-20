934f44358d85bae6b924f1bc54780a794403c20c
Mithran <mithran.sadasivam@gmail.com>
2026-08-20 12:10:51 -0700

    Update dev login token

--- app/src/main/java/com/familytree/familytree/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/familytree/familytree/ui/screens/LoginScreen.kt
@@ -109,7 +109,7 @@ fun LoginScreen(navController: NavController) {
                     onClick = {
                         isLoading = true
                         scope.launch {
-                            repository.saveToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzg3Njg0Mzc1LCJpYXQiOjE3ODcwNzk1NzUsImp0aSI6IjY4ZTg4MzFlZWExMTQwNzc4ZWFmNzE0MWYyY2YwNTE2IiwidXNlcl9pZCI6MSwiZW1haWwiOiJ0ZXN0QGZhbWlseS5jb20ifQ._vr9NFuBvscCBHUeH-ZWxLARQAvcU7fY3NVeJJUX1v4")
+                            repository.saveToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzg3ODU3NjI0LCJpYXQiOjE3ODcyNTI4MjQsImp0aSI6ImZmZmRjMzEyMDhkODRlM2I5M2VlZTMxOWMwMzEyZTg5IiwidXNlcl9pZCI6MSwiZW1haWwiOiJ0ZXN0QGZhbWlseS5jb20ifQ.qo3_SSost1ptjiUHoQuA2aBG0ODlnHw4YQHLGzlc5S4")
                             isLoading = false
                             navController.navigate(Screen.Home.route) {
                                 popUpTo(Screen.Login.route) { inclusive = true }
